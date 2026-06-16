37d035e8bd4ebfd1506e8b685cf5e995f0fb7c11
Clovis Lee <yunle@ucdavis.edu>
2026-06-16 00:03:58 -0700

    cleanup: Remove duplicate task directories from git history

--- task-016-rowhammer/L3/environment/assets/testbench.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-module testbench;
-    reg clk, rst;
-    reg [7:0] secret_020, test_val;
-    wire [15:0] exec_time;
-    clock_glitch dut (.clk(clk), .rst(rst), .secret_key(secret_020), .input_val(test_val), .exec_time(exec_time));
-    initial begin clk = 0; forever #5 clk = ~clk; end
-    initial begin rst = 1; secret_020 = 8'h77; test_val = 8'h00; #10 rst = 0; #100; $display("TIME: %d", exec_time); #10 $finish; end
-endmodule
